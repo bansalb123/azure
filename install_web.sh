5682d0b7d3019e4e4eb8bd03d93e9edd9f09768b
bansalb123 <46219697+bansalb123@users.noreply.github.com>
2020-07-08 13:14:00 +0530

    Update install_web.sh

--- install_web.sh
+++ install_web.sh
@@ -1,2 +1,6 @@
-apt-get update -y && apt-get upgrade -y
-apt-get install -y nginx
+sudo apt-get update -y && apt-get upgrade -y
+sudo apt-get install openjdk-8-jdk -y
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
